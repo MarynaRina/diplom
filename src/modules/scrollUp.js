b1dd74f9a64488528368e6578f0610444c24abe5
Maryna Maksymchuk <risaranma@gmail.com>
2021-07-13 18:53:06 +0300

    create scroll, modal window

--- src/modules/scrollUp.js
+++ src/modules/scrollUp.js
@@ -2,17 +2,25 @@
 'use strict';
 
 const scrollUp = () => {
-    const upBtn = document.querySelector('.up');
-    upBtn.addEventListener('click', () => {
-        const scrollToTop = window.setInterval(() => {
-            const pos = window.pageYOffset;
-            if (pos > 0) {
-                window.scrollTo(0, pos - 20);
-            } else {
-                window.clearInterval(scrollToTop);
-            }
-        }, 2);
+    const upBtn = document.querySelector('.up'),
+        rootElement = document.documentElement;
+    upBtn.style.display = "none";
+    window.addEventListener('scroll', () => {
+        if (document.body.scrollTop > 500 || document.documentElement.scrollTop > 500) {
+            upBtn.style.display = "block";
+        } else {
+            upBtn.style.display = "none";
+        }
     });
+
+    function scrollToTop() {
+        rootElement.scrollTo({
+            top: 0,
+            behavior: "smooth"
+        });
+    }
+    upBtn.addEventListener("click", scrollToTop);
+
 };
 
 export default scrollUp;
