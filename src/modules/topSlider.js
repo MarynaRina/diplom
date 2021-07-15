c67a46458d85fbe50429b8c63390117e88e0a164
Maryna Maksymchuk <risaranma@gmail.com>
2021-07-15 20:00:21 +0300

    create slider

--- src/modules/topSlider.js
+++ src/modules/topSlider.js
@@ -2,7 +2,19 @@
 'use strict';
 
 const topSlider = () => {
+    let slideIndex = 0;
+    const slides = document.querySelectorAll(".item");
 
+    const showSlides = () => {
+        slides.forEach(slide => slide.style.display = "none");
+        slideIndex++;
+        if (slideIndex > slides.length) {
+            slideIndex = 1;
+        }
+        slides[slideIndex - 1].style.display = "block";
+        setTimeout(showSlides, 3000);
+    };
+    showSlides();
 };
 
 export default topSlider;
